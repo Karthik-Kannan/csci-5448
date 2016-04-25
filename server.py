182d5d033ce6059702d6c5ef7be816975f2ba633
Karthik Kannan <kknicks@gmail.com>
2016-04-25 13:01:16 -0600

    Added teacher role

--- server.py
+++ server.py
@@ -37,6 +37,38 @@ Bootstrap(app)
 login_manager.login_view = 'login'
 
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id',db.Integer , primary_key=True)
+    username = db.Column('username', db.String(20), unique=True , index=True)
+    password = db.Column('password' , db.String(10))
+    email = db.Column('email',db.String(50),unique=True , index=True)
+    registered_on = db.Column('registered_on' , db.DateTime)
+
+    def __init__(self , username ,password , email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+
+
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -146,6 +178,11 @@ def listTests():
 def student():
     return render_template('student.html')
 
+@app.route('/teacher', methods=['GET', 'POSTS'])
+@login_required
+def teacher():
+    return render_template('teacher.html')
+
 
 @app.route('/question',methods=['GET', 'POST'])
 @login_required
@@ -159,34 +196,6 @@ def question():
 def questions():
     return render_template('questions.html')
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column('user_id',db.Integer , primary_key=True)
-    username = db.Column('username', db.String(20), unique=True , index=True)
-    password = db.Column('password' , db.String(10))
-    email = db.Column('email',db.String(50),unique=True , index=True)
-    registered_on = db.Column('registered_on' , db.DateTime)
-
-    def __init__(self , username ,password , email):
-        self.username = username
-        self.password = password
-        self.email = email
-        self.registered_on = datetime.utcnow()
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return unicode(self.id)
-
-    def __repr__(self):
-        return '<User %r>' % (self.username)
 
 
 @app.route('/register' , methods=['GET','POST'])
@@ -216,10 +225,14 @@ def login():
     username = request.form['username']
     password = request.form['password']
     registered_user = User.query.filter_by(username=username,password=password).first()
-    print registered_user
+    print registered_user.username
     if registered_user is None:
         flash('Username or Password is invalid' , 'error')
         return redirect(url_for('login'))
+    elif registered_user.username =='teacher':
+        login_user(registered_user)
+        return redirect(request.args.get('teacher') or url_for('teacher'))
+
     login_user(registered_user)
     flash('Logged in successfully')
     return redirect(request.args.get('student') or url_for('student'))
