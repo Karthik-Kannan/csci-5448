f50e7df6e90a203fff1bb36220f3def97bd39e24
nikhil195 <nikhil_195@hotmail.com>
2016-04-25 15:15:04 -0600

    Performance analysis

--- server.py
+++ server.py
@@ -243,7 +243,14 @@ def contact():
     #                   request.form['university'],
     #                   request.form['phone'])
 
-
+@app.route('/performanceAnalysis',methods=['GET','POST'])
+def performanceAnalysis(chartID = 'chart_ID', chart_type = 'bar', chart_height = 350):
+    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+    series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+    title = {"text": 'My Title'}
+    xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+    yAxis = {"title": {"text": 'yAxis Label'}}
+    return render_template('performanceanalysis.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
